b3af28e3162132602406d115e34a9fc191282ccb
nikkonbd <nikkon1998@gmail.com>
2023-04-24 20:20:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+
+const categories = require('./data/catagories.json')
+
+const news = require('./data/news.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Dragon is Running');
+})
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    if (id == 0) {
+        res.send(news);
+    } else {
+        const categoriesNews = news.filter(c => c.category_id === id);
+        res.send(categoriesNews);
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
